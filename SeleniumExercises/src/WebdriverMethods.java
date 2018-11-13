02fbf27bd326f34665b872cab1a54d05efa5195d
Moral <Moral@INFINITY.LOCAL>
2018-11-13 09:30:58 +0200

    Deleted files manually

--- SeleniumExercises/src/WebdriverMethods.java
+++ SeleniumExercises/src/WebdriverMethods.java
@@ -11,14 +11,26 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+
 
 public class WebdriverMethods {
 
     public static WebDriver driver;
     WebDriverWait wait = new WebDriverWait(driver, 10);
     Actions action = new Actions(driver);
-    
+
+    @BeforeClass
+    public static void OpenBrowser()
+    {
+    	 System.setProperty("webdriver.chrome.driver","C:/SeleniumJavaDrivers/chromedriver.exe");
+         driver= new ChromeDriver();   
+	     driver.manage().window().maximize();
+	     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	     driver.get("https://www.imdb.com/");
+    }
+   
     
     
     
